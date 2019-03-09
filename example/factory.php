33c691dbcb0301d97a2efb393e3acd3151a65ba9
JackYang500 <283598349@qq.com>
2019-03-09 11:46:00 +0800

    添加单例模式工厂模式及示例

--- example/factory.php
+++ example/factory.php
@@ -7,10 +7,13 @@
 
 //简单工厂模式（静态工厂模式）
 //
-interface animal{
+interface animal
+{
     public function say();
 }
-class cat implements animal{
+
+class cat implements animal
+{
 
     public function say()
     {
@@ -18,24 +21,30 @@ class cat implements animal{
         // TODO: Implement say() method.
     }
 }
-class dog implements animal{
+
+class dog implements animal
+{
     public function say()
     {
         echo "汪";
         // TODO: Implement say() method.
     }
 }
-class factory{
+
+class factory
+{
     static function createCat()
     {
         return new cat();
     }
+
     static function createDog()
     {
         return new dog();
     }
 }
-$cat=factory::createCat();
+
+$cat = factory::createCat();
 $cat->say();
-$dog=factory::createDog();
+$dog = factory::createDog();
 $dog->say();
--- example/single.php
+++ example/single.php
@@ -5,37 +5,40 @@
  * Time: 10:58
  */
 
-//简单工厂模式（静态工厂模式）
-//
-interface animal{
-    public function say();
-}
-class cat implements animal{
+//单例模式
 
-    public function say()
+class Single
+{
+    private $name;//声明一个私有的实例变量
+
+    private function __construct()
+    {//声明私有构造方法为了防止外部代码使用new来创建对象。
+    }
+
+    static public $instance;//声明一个静态变量（保存在类中唯一的一个实例）
+
+    static public function getInstance()
+    {//声明一个getInstance()静态方法，用于检测是否有实例对象
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function setName($name)
     {
-        echo "喵";
-        // TODO: Implement say() method.
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
     }
 }
-class dog implements animal{
-    public function say()
-    {
-        echo "汪";
-        // TODO: Implement say() method.
-    }
-}
-class factory{
-    static function createCat()
-    {
-        return new cat();
-    }
-    static function createDog()
-    {
-        return new dog();
-    }
-}
-$cat=factory::createCat();
-$cat->say();
-$dog=factory::createDog();
-$dog->say();
+
+$a=Single::getInstance();
+$b=Single::getInstance();
+$a->setName('a');
+$b->setName('b');
+echo $a->getName();
+echo $b->getName();
