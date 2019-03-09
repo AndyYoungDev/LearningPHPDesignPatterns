57df626679d0aabb7610476aa4c9eee4efd22597
JackYang500 <283598349@qq.com>
2019-03-09 11:45:42 +0800

    添加单例模式工厂模式及示例

---
+++ example/single.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * User: Jack Yang
+ * Date: 2019/3/9
+ * Time: 10:58
+ */
+
+//简单工厂模式（静态工厂模式）
+//
+interface animal{
+    public function say();
+}
+class cat implements animal{
+
+    public function say()
+    {
+        echo "喵";
+        // TODO: Implement say() method.
+    }
+}
+class dog implements animal{
+    public function say()
+    {
+        echo "汪";
+        // TODO: Implement say() method.
+    }
+}
+class factory{
+    static function createCat()
+    {
+        return new cat();
+    }
+    static function createDog()
+    {
+        return new dog();
+    }
+}
+$cat=factory::createCat();
+$cat->say();
+$dog=factory::createDog();
+$dog->say();
